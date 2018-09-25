b120ecef0977d4fbc78c9d46e95e9619d942826e
Ru Al <39805456+rualb@users.noreply.github.com>
2018-09-25 19:09:25 +0400

    Update simple-mix.java

--- simple-mix.java
+++ simple-mix.java
@@ -7,6 +7,37 @@ import java.util.*;
 
 public class Main {
 
+        
+        public static void longest_increasing_subsequence_via_dyn() {
+
+        Integer[] arr = {3, 4, -1, 0, 6, 2, 3};
+        Integer[] buf = new Integer[arr.length];
+
+        //init
+        for (int i1 = 0; i1 < arr.length; ++i1) {
+            if (buf[i1] == null)
+                buf[i1] = 1;
+        }
+
+        for (int i1 = 0; i1 < arr.length-1; ++i1) {
+
+            for (int i2 = 0; i2 <= i1; ++i2) {
+
+                if (arr[i1+1] > arr[i2] && buf[i2] + 1 > buf[i1+1])
+                    buf[i1+1] = buf[i2] + 1;
+
+            }
+
+        }
+
+        int max = -1;
+        for (Integer x : buf)
+            max = Math.max(max, x);
+
+        System.out.println(max);
+
+    }
+        
 public static void longest_increasing_subsequence_via_stack(){
 
         Integer[] arr = {3, 4, -1, 0, 6, 2, 3};
